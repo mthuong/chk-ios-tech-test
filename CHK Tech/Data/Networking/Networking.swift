679944b17ea0368079a8f2461d95c06fea10337a
Tom <thuong.nguyen@itads.net>
2021-09-25 18:34:39 +0700

    feat: cleanup code

--- CHK Tech/Data/Networking/Networking.swift
+++ CHK Tech/Data/Networking/Networking.swift
@@ -14,8 +14,6 @@ protocol NetworkingProtocol: AnyObject {
     func get<T>(type: T.Type,
                 url: URL,
                 headers: Headers) -> AnyPublisher<T, Error> where T: Decodable
-    
-    func getData(url: URL, headers: Headers) -> AnyPublisher<Data, URLError>
 }
 
 final class Networking: NetworkingProtocol {
@@ -37,28 +35,4 @@ final class Networking: NetworkingProtocol {
             .decode(type: T.self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
-    
-    func getData(url: URL, headers: Headers) -> AnyPublisher<Data, URLError> {
-        
-        var urlRequest = URLRequest(url: url)
-        
-        headers.forEach { key, value in
-            if let value = value as? String {
-                urlRequest.setValue(value, forHTTPHeaderField: key)
-            }
-        }
-        
-        return URLSession.shared.dataTaskPublisher(for: urlRequest)
-            .map(\.data)
-            .eraseToAnyPublisher()
-    }
-}
-
-func decode<T: Decodable>(_ data: Data) -> AnyPublisher<T, Error> {
-  let decoder = JSONDecoder()
-  decoder.dateDecodingStrategy = .secondsSince1970
-
-  return Just(data)
-    .decode(type: T.self, decoder: decoder)
-    .eraseToAnyPublisher()
 }
